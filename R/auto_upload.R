c9152a9a9324654ba25df4dc34274b345f960af1
Yusri <yusriyp@gmail.com>
2015-08-31 11:50:29 +0800

    Script to auto upload the website code generated from Rmarkdown.

---
+++ R/auto_upload.R
@@ -0,0 +1,11 @@
+
+## To run the Rmarkdown script
+library(knitr)
+library(markdown)
+library(RCurl)
+
+
+knit2html('R/website_code.Rmd')
+
+rpubsUpload(title='IPULAUPI4',htmlFile = 'website_code.html',
+            method = getOption("rpubs.upload.method", "curl"))
