273205e46ae34cba8213ca10ac75d143e4969a97
yusriy <yusriyp@gmail.com>
2015-08-31 18:06:35 +0800

    Some minor corrections.

--- R/auto_upload.R
+++ R/auto_upload.R
@@ -4,17 +4,16 @@
 # 1. Load needed libraries
 library(knitr) # To use the function `knit2html`
 library(markdown) # To use the function `rpubsUpload`
-library(RCurl)
+library(RCurl) # Not used at the momemt
 library(RSelenium) # To download and submit online forms
 
 # 2. Process the knitr file
 knit2html('R/website_code.Rmd')
 
-# 3. Upload the generated file, need to copy the 'continueURL' and paste in a 
-# browser to complete the upload
+# 3. Upload the generated file
 result <- rpubsUpload(title='IPULAUPI4',htmlFile = 'R/website_code.html',
                       method = getOption("rpubs.upload.method", "internal"))
-form <- getForm(result$continueUrl, btnG = 'Continue')
+form <- postForm(result$continueUrl)
 
-
+# To view the website, need to fill in the details (optional) and press Enter
 browseURL(result$continueUrl)
