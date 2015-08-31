e7c3832eda21b2f261334e8323614e56ef114b0a
yusriy <yusriyp@gmail.com>
2015-08-31 13:56:29 +0800

    Attempt to auto upload Rmarkdown to Rpubs

--- R/auto_upload.R
+++ R/auto_upload.R
@@ -1,11 +1,20 @@
 
 ## To run the Rmarkdown script
-library(knitr)
-library(markdown)
+
+# 1. Load needed libraries
+library(knitr) # To use the function `knit2html`
+library(markdown) # To use the function `rpubsUpload`
 library(RCurl)
+library(RSelenium) # To download and submit online forms
 
-
+# 2. Process the knitr file
 knit2html('R/website_code.Rmd')
 
-rpubsUpload(title='IPULAUPI4',htmlFile = 'website_code.html',
-            method = getOption("rpubs.upload.method", "auto"))
+# 3. Upload the generated file, need to copy the 'continueURL' and paste in a 
+# browser to complete the upload
+result <- rpubsUpload(title='IPULAUPI4',htmlFile = 'R/website_code.html',
+                      method = getOption("rpubs.upload.method", "internal"))
+form <- getForm(result$continueUrl, btnG = 'Continue')
+
+
+browseURL(result$continueUrl)
