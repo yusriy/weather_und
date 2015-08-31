4b6cd3658543108e7b118648c22cdb372db8f577
Yusri <yusriyp@gmail.com>
2015-08-31 11:51:34 +0800

    Nothing much

--- R/auto_upload.R
+++ R/auto_upload.R
@@ -8,4 +8,4 @@ library(RCurl)
 knit2html('R/website_code.Rmd')
 
 rpubsUpload(title='IPULAUPI4',htmlFile = 'website_code.html',
-            method = getOption("rpubs.upload.method", "curl"))
+            method = getOption("rpubs.upload.method", "auto"))
